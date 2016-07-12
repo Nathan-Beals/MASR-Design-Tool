4fe40d231dde61a7f1bb4181be1c329f78b4e282
Devan Thanki <dthanki3@ad.gatech.edu>
2016-07-12 11:07:15 -0400

    Equations updated via Python
    Equations are updated via Python and the model is saved and a copy is
    placed in the Output folder

--- masr_design_tool/buildmodel.py
+++ masr_design_tool/buildmodel.py
@@ -23,7 +23,7 @@ def build_model(quad,cover_flag, alt_infovar):
     try:
         import pythoncom
     except ImportError:
-        main_GUI.AlternativesFrame.alt_infovar.set("pythoncom module not installed for export.")
+        alt_infovar.set("pythoncom module not installed for export.")
         return
     
     # Initialize an instance of SolidWorks 201X
@@ -41,14 +41,14 @@ def build_model(quad,cover_flag, alt_infovar):
     
     src_files = os.listdir(os.path.abspath(os.path.join('model/')))
     
-    for file_name in src_files:
-            full_file_name = os.path.join(os.path.abspath(os.path.join('model/')), file_name)
-            shutil.copy(full_file_name, ModelDataDir)
+#     for file_name in src_files:
+#             full_file_name = os.path.join(os.path.abspath(os.path.join('model/')), file_name)
+#             shutil.copy(full_file_name, ModelDataDir)
     
     swApp = win32com.client.Dispatch("SldWorks.Application")
     
     # Open the multirotor model
-    modelPath = os.path.abspath(os.path.join(ModelDataDir, 'Quad Assemb.SLDASM'))
+    modelPath = os.path.abspath(os.path.join("model/", 'Quad Assemb.SLDASM'))
     longstatus = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, -1)
     longwarnings = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, -1)
     swApp.OpenDoc6(modelPath, 2, 0, "", longstatus, longwarnings)
@@ -56,9 +56,11 @@ def build_model(quad,cover_flag, alt_infovar):
     # Make the model the active model
     swApp.ActivateDoc2("Quad Assemb.SLDASM", False, longstatus)
     model = swApp.ActiveDoc
+    modelExt = model.Extension
     
     # Assign Equation Manager to handle the parameters
     eqMgr = model.GetEquationMgr
+    
     
 #     count = eqMgr.GetCount
     
@@ -72,8 +74,8 @@ def build_model(quad,cover_flag, alt_infovar):
     plate_xdim = 4.25 # inches
     plate_ydim = 5.75 # inches
     plate_thickness = 0.1 #inches
-    bat_xdim = convert_unit(quad.battery.xdim['value'], 'm', 'in')
-    bat_ydim = convert_unit(quad.battery.ydim['value'], 'm', 'in')
+    bat_xdim = convert_unit(quad.battery.ydim['value'], 'm', 'in') #X and Y dimensions are interchanged
+    bat_ydim = convert_unit(quad.battery.xdim['value'], 'm', 'in')
     bat_zdim = convert_unit(quad.battery.zdim['value'], 'm', 'in')
     print bat_xdim, bat_ydim, bat_zdim
     arm_length = convert_unit(quad.arm_len['value'], 'm', 'in')
@@ -105,46 +107,90 @@ def build_model(quad,cover_flag, alt_infovar):
     # motor_len =
     # motor_shaft_dia =  
     
-    eqMgr.Equation(0, "\"Propeller Size\"= " + str(prop_dia))
-    eqMgr.Equation(1, "\"Plate Dim X\"= " + str(plate_xdim)+ "in")
-    eqMgr.Equation(2, "\"Plate Dim Y\"= " + str(plate_ydim)+ "in")
-    eqMgr.Equation(4, "\"Plate Thickness\"= " + str(plate_thickness)+ "in")
-    eqMgr.Equation(5, "\"Batt X\"= " + str(bat_xdim)+ "in")
-    eqMgr.Equation(6, "\"Batt Y\"= " + str(bat_ydim)+ "in")
-    eqMgr.Equation(7, "\"Batt Z\"= " + str(bat_zdim)+ "in")
-    eqMgr.Equation(8, "\"Arm Length\"= " + str(arm_len)+ "in")
-    eqMgr.Equation(9, "\"ESC Y\"= " + str(2.4)+ "in") # temp value
-    eqMgr.Equation(10, "\"ESC X\"= " + str(26) + "mm") # temp value
-    eqMgr.Equation(11, "\"ESC Z\"= " + str(10.5) + "mm") # temp value
-    eqMgr.Equation(12, "\"Component Y\"= " + str(1.64)+ "in")
-    eqMgr.Equation(13, "\"Dowell Height\"= " + str(0.1)+ "in")
-    eqMgr.Equation(14, "\"Dowell Width\"= " + str(0.25)+ "in")
-    eqMgr.Equation(15, "\"Motor Screw Y\"= " + str(25)+ "mm")
-    eqMgr.Equation(16, "\"Motor Screw X\"= " + str(19)+ "mm")
-    eqMgr.Equation(17, "\"Gimbal Mount\"= " + str(0))
-    eqMgr.Equation(18, "\"Payload Bot Z\"= " + str(0))
-    eqMgr.Equation(19, "\"Motor Screw Diam\"= " + str(3)+ "mm")
-    eqMgr.Equation(20, "\"GPS X\"= " + str(2)+ "in")
-    eqMgr.Equation(21, "\"GPS y\"= " + str(2)+ "in")
-    eqMgr.Equation(22, "\"GPS Z\"= " + str(0.5)+ "in")
-    eqMgr.Equation(23, "\"Flight Controller X\"= " + str(2)+ "in")
-    eqMgr.Equation(24, "\"Flight Controller Y\"= " + str(3.25)+ "in")
-    eqMgr.Equation(25, "\"Flight Controller Z\"= " + str(0.5)+ "in")
-    eqMgr.Equation(26, "\"Radio Rx X\"= " + str(1.13)+ "in")
-    eqMgr.Equation(27, "\"Radio Rx Y\"= " + str(2)+ "in")
-    eqMgr.Equation(28, "\"Radio Rx Z\"= " + str(0.5)+ "in")
-    eqMgr.Equation(30, "\"Cover\"= " + str(cover))
-    eqMgr.Equation(33, "\"Motor Diam\"= " + str(motor_dia)+ "in")
-    eqMgr.Equation(34, "\"Motor Length\"= " + str(23)+ "mm")
-    eqMgr.Equation(35, "\"Motor Shaft Diam\"= " + str(3)+ "mm")
-    # Rebuild the model so the new parameters take effect
+    file = open(os.path.abspath(os.path.join("model/", 'equations.txt')),'w')
+    
+    file.write( "\"Propeller Size\"= " + str(prop_dia) + "in" + '\n')
+    file.write( "\"Plate Dim X\"= " + str(plate_xdim)+ "in" + '\n')
+    file.write( "\"Plate Dim Y\"= " + str(plate_ydim)+ "in" + '\n')
+    file.write( "\"D2@Sketch1\"=\"Propeller Size\"" + '\n')
+    file.write( "\"Plate Thickness\"= " + str(plate_thickness)+ "in" + '\n')
+    file.write( "\"Batt X\"= " + str(bat_xdim)+ "in" + '\n')
+    file.write( "\"Batt Y\"= " + str(bat_ydim)+ "in" + '\n')
+    file.write( "\"Batt Z\"= " + str(bat_zdim)+ "in" + '\n')
+    file.write( "\"Arm Length\"= \"Propeller Size\" * .357 + 2.965" + '\n')
+    file.write( "\"ESC Y\"= " + str(2.4)+ "in" + '\n') # temp value
+    file.write( "\"ESC X\"= " + str(26) + "mm" + '\n') # temp value
+    file.write( "\"ESC Z\"= " + str(10.5) + "mm" + '\n') # temp value
+    file.write( "\"Component Y\"= " + str(1.64)+ "in" + '\n')
+    file.write( "\"Dowell Height\"= " + str(0.1) + '\n')
+    file.write( "\"Dowell Width\"= " + str(0.25) + '\n')
+    file.write( "\"Motor Screw Y\"= " + str(25)+ "mm" + '\n')
+    file.write( "\"Motor Screw X\"= " + str(19)+ "mm" + '\n')
+    file.write( "\"Gimbal Mount\"= " + str(0)+ '\n')
+    file.write( "\"Payload Bot Z\"= " + str(0)+ '\n')
+    file.write( "\"Motor Screw Diam\"= " + str(3)+ "mm" + '\n')
+    file.write( "\"GPS X\"= " + str(2) + '\n')
+    file.write( "\"GPS Y\"= " + str(2) + '\n')
+    file.write( "\"GPS Z\"= " + str(0.5) + '\n')
+    file.write( "\"Flight Controller X\"= " + str(2) + '\n')
+    file.write( "\"Flight Controller Y\"= " + str(3.25) + '\n')
+    file.write( "\"Flight Controller Z\"= " + str(0.5) + '\n')
+    file.write( "\"Radio Rx X\"= " + str(1.13) + '\n')
+    file.write( "\"Radio Rx Y\"= " + str(2) + '\n')
+    file.write( "\"Radio Rx Z\"= " + str(0.5) + '\n')
+    file.write( "\"D1@Sketch1\"=\"Propeller Size\"/2+4" + '\n')
+    file.write( "\"Cover\"= " + str(cover) + '\n')
+    file.write( "\"Top Plate-1.Part\"= if ( \"Cover\" = 0 , \"unsuppressed\" , \"suppressed\" )" + '\n')
+    file.write( "\"Top Plate Cover_new-1.Part\"= if ( \"Cover\" = 1 , \"unsuppressed\" , \"suppressed\" )" + '\n')
+    file.write( "\"Motor Diam\"= " + str(motor_dia)+ "in" + '\n')
+    file.write( "\"Motor Length\"= " + str(23)+ "mm" + '\n')
+    file.write( "\"Motor Shaft Diam\"= " + str(3)+ "mm" + '\n')
+    file.close()
     boolstatus = model.EditRebuild3
+    
+    
+#     eqMgr.Equation(0, "\"Propeller Size\"= " + str(prop_dia))
+#     eqMgr.Equation(1, "\"Plate Dim X\"= " + str(plate_xdim)+ "in")
+#     eqMgr.Equation(2, "\"Plate Dim Y\"= " + str(plate_ydim)+ "in")
+#     eqMgr.Equation(4, "\"Plate Thickness\"= " + str(plate_thickness)+ "in")
+#     eqMgr.Equation(5, "\"Batt X\"= " + str(bat_xdim)+ "in")
+#     eqMgr.Equation(6, "\"Batt Y\"= " + str(bat_ydim)+ "in")
+#     eqMgr.Equation(7, "\"Batt Z\"= " + str(bat_zdim)+ "in")
+#     eqMgr.Equation(8, "\"Arm Length\"= " + str(arm_len)+ "in")
+#     eqMgr.Equation(9, "\"ESC Y\"= " + str(2.4)+ "in") # temp value
+#     eqMgr.Equation(10, "\"ESC X\"= " + str(26) + "mm") # temp value
+#     eqMgr.Equation(11, "\"ESC Z\"= " + str(10.5) + "mm") # temp value
+#     eqMgr.Equation(12, "\"Component Y\"= " + str(1.64)+ "in")
+#     eqMgr.Equation(13, "\"Dowell Height\"= " + str(0.1)+ "in")
+#     eqMgr.Equation(14, "\"Dowell Width\"= " + str(0.25)+ "in")
+#     eqMgr.Equation(15, "\"Motor Screw Y\"= " + str(25)+ "mm")
+#     eqMgr.Equation(16, "\"Motor Screw X\"= " + str(19)+ "mm")
+#     eqMgr.Equation(17, "\"Gimbal Mount\"= " + str(0))
+#     eqMgr.Equation(18, "\"Payload Bot Z\"= " + str(0))
+#     eqMgr.Equation(19, "\"Motor Screw Diam\"= " + str(3)+ "mm")
+#     eqMgr.Equation(20, "\"GPS X\"= " + str(2)+ "in")
+#     eqMgr.Equation(21, "\"GPS Y\"= " + str(2)+ "in")
+#     eqMgr.Equation(22, "\"GPS Z\"= " + str(0.5)+ "in")
+#     eqMgr.Equation(23, "\"Flight Controller X\"= " + str(2)+ "in")
+#     eqMgr.Equation(24, "\"Flight Controller Y\"= " + str(3.25)+ "in")
+#     eqMgr.Equation(25, "\"Flight Controller Z\"= " + str(0.5)+ "in")
+#     eqMgr.Equation(26, "\"Radio Rx X\"= " + str(1.13)+ "in")
+#     eqMgr.Equation(27, "\"Radio Rx Y\"= " + str(2)+ "in")
+#     eqMgr.Equation(28, "\"Radio Rx Z\"= " + str(0.5)+ "in")
+#     eqMgr.Equation(30, "\"Cover\"= " + str(cover))
+#     eqMgr.Equation(33, "\"Motor Diam\"= " + str(motor_dia)+ "in")
+#     eqMgr.Equation(34, "\"Motor Length\"= " + str(23)+ "mm")
+#     eqMgr.Equation(35, "\"Motor Shaft Diam\"= " + str(3)+ "mm")
+    # Rebuild the model so the new parameters take effect
+#     boolstatus = model.EditRebuild3
 #     print boolstatus
 #     print "*****"
 #     for i in range(0, count):
 #         print eqMgr.Equation(i)
     
+    longstatus = model.SaveAs3(modelPath, 0, 0) 
     # Toggle the "save as single STL" preference off
+    
     arg1 = win32com.client.VARIANT(pythoncom.VT_I4, 72)
     argBool = win32com.client.VARIANT(pythoncom.VT_BOOL, False)
     swApp.SetUserPreferenceToggle(arg1, argBool)
@@ -158,13 +204,15 @@ def build_model(quad,cover_flag, alt_infovar):
     longstatus = model.SaveAs3(ModelDataDir + "\Multirotor.STL", 0, 0)
     # Save a single .SLDASM of the whole model for viewing purposes
     longstatus = model.SaveAs3(ModelDataDir + "\Multirotor.SLDASM", 0, 0)
-#    Toggle the "save as single STL" preference back off  
+    longstatus = model.SaveAs3(ModelDataDir + "\Quad Assemb.SLDASM", 0, 0)
+    # Toggle the "save as single STL" preference back off  
     argBool = win32com.client.VARIANT(pythoncom.VT_BOOL, False)
     swApp.SetUserPreferenceToggle(arg1, argBool)
-    
-    
-    
+     
+     
+     
     # Close the model
     model = None
-    swApp.CloseDoc("Multirotor.SLDASM")
-    swApp.ExitApp()
+#     swApp.CloseDoc("Quad Assemb.SLDASM")
+    #swApp.ExitApp()
+    alt_infovar.set("Model Built!")
